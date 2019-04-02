b14ba01ca279554c498b5119d5be112f7315d1b1
Teseo Schneider <teseo@bluewin.ch>
2019-04-02 19:07:29 -0400

    project done, problems with compilation

---
+++ cmake/PyiglDependencies.cmake
@@ -0,0 +1,30 @@
+# Prepare dependencies
+#
+# For each third-party library, if the appropriate target doesn't exist yet,
+# download it via external project, and add_subdirectory to build it alongside
+# this project.
+
+### Configuration
+set(LIBIGL_ROOT     "${CMAKE_CURRENT_LIST_DIR}/..")
+
+# Download and update 3rdparty libraries
+list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR})
+list(REMOVE_DUPLICATES CMAKE_MODULE_PATH)
+include(PyiglDownloadExternal)
+
+################################################################################
+# Required libraries
+################################################################################
+
+SET(NPE_WITH_EIGEN ${LIBIGL_EXTERNAL}/libigl/external/eigen)
+
+SET(LIBIGL_EIGEN_VERSION "3.3.7")
+SET(LIBIGL_EIGEN_MD5 "f2a417d083fe8ca4b8ed2bc613d20f07")
+
+pyigl_download_igl()
+pyigl_download_numpyeigen()
+list(APPEND CMAKE_MODULE_PATH ${LIBIGL_EXTERNAL}/libigl/cmake)
+include(LibiglDownloadExternal)
+igl_download_tutorial_data()
+
+
---
+++ cmake/PyiglDownloadExternal.cmake
@@ -0,0 +1,52 @@
+################################################################################
+include(DownloadProject)
+
+# With CMake 3.8 and above, we can hide warnings about git being in a
+# detached head by passing an extra GIT_CONFIG option
+if(NOT (${CMAKE_VERSION} VERSION_LESS "3.8.0"))
+    set(pyigl_EXTRA_OPTIONS "GIT_CONFIG advice.detachedHead=false")
+else()
+    set(pyigl_EXTRA_OPTIONS "")
+endif()
+
+# Shortcut function
+function(pyigl_download_project name)
+    download_project(
+        PROJ         ${name}
+        SOURCE_DIR   ${LIBIGL_EXTERNAL}/${name}
+        DOWNLOAD_DIR ${LIBIGL_EXTERNAL}/.cache/${name}
+        QUIET
+        ${pyigl_EXTRA_OPTIONS}
+        ${ARGN}
+    )
+endfunction()
+
+################################################################################
+
+## libigl
+function(pyigl_download_igl)
+    pyigl_download_project(libigl
+        GIT_REPOSITORY https://github.com/libigl/libigl.git
+        GIT_TAG        9230cc8b83335e7146a974d1ac3aa1a3a90a7602
+    )
+endfunction()
+
+
+## numpyeigen
+function(pyigl_download_numpyeigen)
+    pyigl_download_project(numpyeigen
+        GIT_REPOSITORY https://github.com/fwilliams/numpyeigen.git
+        GIT_TAG        0c8aac54eedc5cfab0d8723081dd6ea727ce4806
+    )
+endfunction()
+
+
+
+# ## Sanitizers
+# function(pyigl_download_sanitizers)
+#     pyigl_download_project(sanitizers-cmake
+#         GIT_REPOSITORY https://github.com/arsenm/sanitizers-cmake.git
+#         GIT_TAG        6947cff3a9c9305eb9c16135dd81da3feb4bf87f
+#     )
+# endfunction()
+
