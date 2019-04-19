f8a4443bfa1811905565fbcaa46f14b940abc92e
Teseo Schneider <teseo@bluewin.ch>
2019-04-19 11:38:57 -0400

    llvm on windows1

--- setup.py
+++ setup.py
@@ -58,6 +58,10 @@ class CMakeBuild(build_ext):
         env = os.environ.copy()
         env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),self.distribution.get_version())
 
+        print("asdasdasd-------------------")
+        print(['cmake', ext.sourcedir] + cmake_args)
+        print("asdasdasd-------------------")
+
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
         subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
