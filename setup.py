44a9f6fef34a80cbbfd3704fa464e09892f9bf95
Teseo Schneider <teseo@bluewin.ch>
2019-04-19 14:18:08 -0400

    llvm on windows4

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ import sys
 import platform
 import subprocess
 
+import sysconfig
+
 from distutils.version import LooseVersion
 from setuptools import setup, Extension, find_packages
 from setuptools.command.build_ext import build_ext
@@ -24,7 +26,7 @@ class CMakeBuild(build_ext):
             raise RuntimeError(
                 "CMake must be installed to build the following extensions: , ".join(e.name for e in self.extensions))
 
-        self.debug = True
+        # self.debug = True
 
         cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)', out.decode()).group(1))
         if cmake_version < '3.2.0':
@@ -51,6 +53,7 @@ class CMakeBuild(build_ext):
             if sys.maxsize > 2**32:
                 cmake_args += ['-A', 'x64']
             cmake_args += ['-T', 'llvm']
+            cmake_args += ['-D', 'PYTHON_LIBRARY={}\\libs\\python{}.lib'.format(sysconfig.get_path('data'),  sysconfig.get_config_var('py_version_short').replace('.',''))]
             build_args += ['--', '/m']
         else:
             build_args += ['--', '-j2']
