e50eed3d0ef1947075b6d5904b00e5b821e940c5
Teseo Schneider <teseo@bluewin.ch>
2019-04-19 17:09:59 -0400

    trying python in windows

--- setup.py
+++ setup.py
@@ -11,6 +11,9 @@ from distutils.version import LooseVersion
 from setuptools import setup, Extension, find_packages
 from setuptools.command.build_ext import build_ext
 
+python_lib = None
+python_lib_debug = None
+
 
 class CMakeExtension(Extension):
     def __init__(self, name, sourcedir=''):
@@ -48,18 +51,32 @@ class CMakeBuild(build_ext):
         build_args = ['--config', cfg]
         cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
 
+        python_libs = ""
+
+        if python_lib:
+            python_libs += python_lib
+        if python_lib_debug:
+            if python_libs:
+                python_libs += "\\;" + python_lib_debug
+            else:
+                python_libs += python_lib_debug
+
         if platform.system() == "Windows":
             cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
             if sys.maxsize > 2**32:
                 cmake_args += ['-A', 'x64']
             cmake_args += ['-T', 'llvm']
-            if self.debug:
-                cmake_args += ['-D', 'PYTHON_LIBRARY={}\\libs\\python{}.lib\\;PYTHON_LIBRARY={}\\libs\\python{}_d.lib'.format(sysconfig.get_path('data'),  sysconfig.get_config_var('py_version_short').replace('.',''), sysconfig.get_path('data'),  sysconfig.get_config_var('py_version_short').replace('.',''))]
-            else:
-                cmake_args += ['-D', 'PYTHON_LIBRARY={}\\libs\\python{}.lib'.format(sysconfig.get_path('data'),  sysconfig.get_config_var('py_version_short').replace('.',''))]
             build_args += ['--', '/m']
         else:
             build_args += ['--', '-j2']
+
+
+        if python_libs:
+                cmake_args += ['-D', 'PYTHON_LIBRARY={}'.format(python_libs)]
+
+        print("Cmake----------------")
+        print(cmake_args)
+        print("Cmake----------------")
 
         env = os.environ.copy()
         env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),self.distribution.get_version())
@@ -75,6 +92,18 @@ class CMakeBuild(build_ext):
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
+
+if "--python-lib" in sys.argv:
+    index = sys.argv.index('--python-lib')
+    sys.argv.pop(index)  # Removes the '--python-lib'
+    python_lib = sys.argv.pop(index)
+
+
+if "--python-lib-debug" in sys.argv:
+    index = sys.argv.index('--python-lib-debug')
+    sys.argv.pop(index)  # Removes the '--python-lib-debug'
+    python_lib_debug = sys.argv.pop(index)
 
 
 setup(
