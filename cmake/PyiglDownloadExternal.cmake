c3c36122e1f403a2fac593b312516348303ec275
Teseo Schneider <teseo@bluewin.ch>
2019-04-24 11:33:33 -0400

    updated libigl version

--- cmake/PyiglDownloadExternal.cmake
+++ cmake/PyiglDownloadExternal.cmake
@@ -27,7 +27,7 @@ endfunction()
 function(pyigl_download_igl)
     pyigl_download_project(libigl
         GIT_REPOSITORY https://github.com/skoch9/libigl
-        GIT_TAG        73ce4ec4ba7b4bcf5b9303be03da8d5a92fd4029
+        GIT_TAG        a51b697fe24042f92a4bb089a9ad2c8e27b4d724
     )
 endfunction()
 
