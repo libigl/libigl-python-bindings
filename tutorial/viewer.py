90a509ca224cf9505bac9c7adf8ceaa6de7a63c6
Sebastian Koch <s.koch@phusion.eu>
2019-05-05 14:16:51 +0200

    Refactor/Cleanup viewer, Update tutorials

--- tutorial/viewer.py
+++ tutorial/viewer.py
@@ -9,7 +9,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Helper functions
-def get_colors(inp, colormap=plt.cm.viridis, normalize=True, vmin=None, vmax=None):
+def get_colors(inp, colormap="viridis", normalize=True, vmin=None, vmax=None):
+    colormap = plt.cm.get_cmap(colormap)
     if normalize:
         vmin=np.min(inp)
         vmax=np.max(inp)
@@ -20,40 +21,36 @@ def get_colors(inp, colormap=plt.cm.viridis, normalize=True, vmin=None, vmax=Non
 
 
 class Viewer():
-    def __init__(self):
+    def __init__(self, shading={}):
+        self.update_shading(shading)
         #print("Viewer initialization")
         self.data = {}
-        self.width = 750
-        self.height = 750
+        self.width = self.s["width"]
+        self.height = self.s["height"]
         self.light = DirectionalLight(color='white', position=[0, 0, 1], intensity=0.6)
-        self.cam = PerspectiveCamera(position=[0, 0, 1], lookAt=[0, 0, 0], fov=30, aspect=self.width/self.height,
-                        children=[self.light])
+        self.cam = PerspectiveCamera(position=[0, 0, 1], lookAt=[0, 0, 0], fov=30, 
+                                     aspect=self.width/self.height,
+                                     children=[self.light])
         self.orbit = OrbitControls(controlling=self.cam)
         self.renderer = Renderer(camera=self.cam, scene = Scene(), controls=[self.orbit], 
-                    width=self.width, height=self.height, antialias=True)
+                    width=self.width, height=self.height, antialias=self.s["antialias"])
         self.widgets = []
-    
-    def normalize_mesh(self, v):
-        mi = np.min(v)
-        ma = np.max(v)
-        self.min = mi
-        self.max = ma
+        
+    def update_shading(self, shading):
+        shad = {"flat":True, "wireframe":True, "wire_width": 0.03, "wire_color": "black",
+                "width": 600, "height": 600, "antialias": True, "scale": 2.3, "side": 'DoubleSide',
+                "colormap": "viridis", "normalize": [None, None], "background": "#ffffff",
+                "line_width": 1.0, "line_color": "black", "bbox": False, 
+                "point_color": "red", "point_size": 0.01
+               }
+        for k in shading:
+            shad[k] = shading[k]
+        self.s = shad
 
-        v = (v - mi) / ((ma - mi) * 2)
-        mi = np.min(v)
-        ma = np.max(v)
-        mean = v.mean(0)
-        v -= mean
-        self.mean = mean
-        return v
+    def set_mesh(self, v, f, c=None, shading={}):
+        self.update_shading(shading)
 
-    def set_mesh(self, v, f, c=None, shading={"flat": True, "wireframe": True, "wire_width":0.03}):
-        self.shading = shading
-        #v = self.normalize_mesh(v)
-        #ma = np.max(v)
-        #mi = np.min(v)
-        #vi = v/ ((ma - mi) * 2)
-        self.scale =  2.3 * (np.max(v) - np.min(v))
+        self.scale = self.s["scale"] * (np.max(v) - np.min(v))
         mean = v.mean(0)# * self.scale
         mean = mean.tolist()
         self.orbit.target = mean
@@ -75,6 +72,9 @@ class Viewer():
                 color=BufferAttribute(c)
             ))
         else:
+            normalize = self.s["normalize"][0] != None and self.s["normalize"][1] != None
+            c = get_colors(c, self.s["colormap"], normalize=normalize, 
+                       vmin=self.s["normalize"][0], vmax=self.s["normalize"][1])
             c = c.astype("float32", copy=False)
             c = c[:, :3]
             geometry = BufferGeometry(attributes=dict(
@@ -82,25 +82,28 @@ class Viewer():
                 index=BufferAttribute(f, normalized=False),
                 color=BufferAttribute(c),
             ))
+        
+        self.meshmaterial = MeshStandardMaterial(vertexColors='VertexColors', 
+                            reflectivity=1.0, side=self.s["side"], 
+                            roughness=0.5, metalness=0.25, flatShading=self.s["flat"],
+                            polygonOffset=True, polygonOffsetFactor= 1, polygonOffsetUnits=1)
         geometry.exec_three_obj_method('computeVertexNormals')
-        self.mesh = Mesh(geometry=geometry, material=MeshStandardMaterial(vertexColors='VertexColors', reflectivity=1.0, 
-                                                            #side='FrontSide', 
-                                                            roughness=0.5, metalness=0.25, flatShading=self.shading["flat"],
-                                                            polygonOffset=True, polygonOffsetFactor= 1,
-                                                            polygonOffsetUnits=1))
-#MeshLambertMaterial(vertexColors='VertexColors')
-        if self.shading["wireframe"]:
+        self.mesh = Mesh(geometry=geometry, material=self.meshmaterial)
+        
+        #MeshLambertMaterial(vertexColors='VertexColors')
+        if self.s["wireframe"]:
             self.geon = WireframeGeometry(self.mesh.geometry) # WireframeGeometry
-            mat = LineBasicMaterial(color="black", linewidth=self.shading["wire_width"])
+            mat = LineBasicMaterial(color=self.s["wire_color"], linewidth=self.s["wire_width"])
             self.wireframe = LineSegments(self.geon, mat )
             self.mesh.add(self.wireframe)
+        
+        if self.s["bbox"]:
+            self.add_bbox()
 
-
-        #self.mesh.scale = (scale, scale, scale)
-        self.scene = Scene(children=[self.mesh, self.cam, AmbientLight(intensity=0.5)], background="#ffffff")#"#4c4c80"
+        self.scene = Scene(children=[self.mesh, self.cam, AmbientLight(intensity=0.5)], 
+                           background=self.s["background"])#"#4c4c80"
         self.renderer.scene = self.scene
-        #self.mesh.scale = (self.scale, self.scale, self.scale)
-        #print(self.scale)
+
         self.orbit.exec_three_obj_method('update')
         self.cam.exec_three_obj_method('updateProjectionMatrix')
         
@@ -115,35 +118,79 @@ class Viewer():
         self.update()
 
         
-    def set_colors(self, c):
+    def update_colors(self, c):
+        normalize = self.s["normalize"][0] != None and self.s["normalize"][1] != None
+        c = get_colors(c, self.s["colormap"], normalize=normalize, 
+                   vmin=self.s["normalize"][0], vmax=self.s["normalize"][1])
         c = c.astype("float32", copy=False)
         c = c[:, :3]
-        self.mesh.geometry.attributes.get("color").array = c
+        self.mesh.geometry.attributes["color"].array = c
+        self.mesh.geometry.attributes["color"].needsUpdate = True
+        self.mesh.geometry.verticesNeedUpdate = True
+        self.mesh.geometry.elementsNeedUpdate = True
+        self.update()
         
     def add_lines(self, beginning, ending, shading={}, update=True):#, colors):
-        shad = {"width":1, "color": "black"}
-        for k in shading:
-            shad[k] = shading[k]
-        shading = shad
+        self.update_shading(shading)
         lines = np.hstack([beginning, ending])
         lines = lines.reshape((-1, 3))
-        #lines = (lines - self.min) / ((self.max - self.min) * 2)
-        #lines -= self.mean
         lines = lines.astype("float32", copy=False)
-        g1 = BufferGeometry(
-            attributes={
+        g1 = BufferGeometry(attributes={
                 'position': BufferAttribute(lines, normalized=False),
-            },
-        )
+            },)
 
         #mat = LineBasicMaterial(color="black", linewidth=0.6)
         #linesgeom = Geometry(vertices=lines.tolist())#, colors = colors)# ['red', 'red', 'green', 'green', 'white', 'orange'])
         lines = LineSegments(geometry=g1,#linesgeom, 
-             material=LineBasicMaterial(linewidth=shading["width"], color=shading["color"]))#,#vertexColors='VertexColors'), 
+             material=LineBasicMaterial(linewidth=self.s["line_width"], color=self.s["line_color"]))#,#vertexColors='VertexColors'), 
              #type='LinePieces')
         self.mesh.add(lines)
         #display(self.renderer)
         #self.renderer.exec_three_obj_method('update')
+        if update:
+            self.update()
+            
+    def add_bbox(self, shading={}, update=True):
+        v = self.mesh.geometry.attributes["position"].array
+        m = np.min(v, axis=0)
+        M = np.max(v, axis=0)
+
+        # Corners of the bounding box
+        v_box = np.array([[m[0], m[1], m[2]], [M[0], m[1], m[2]], [M[0], M[1], m[2]], [m[0], M[1], m[2]],
+                          [m[0], m[1], M[2]], [M[0], m[1], M[2]], [M[0], M[1], M[2]], [m[0], M[1], M[2]]])
+
+        f_box = np.array([[0, 1], [1, 2], [2, 3], [3, 0], [4, 5], [5, 6], [6, 7], [7, 4],
+                    [0, 4], [1, 5], [2, 6], [7, 3]], dtype=np.int)
+        self.add_edges(v_box, f_box, shading, update)
+        self.add_points(v_box, shading, update)
+
+    def add_points(self, points, shading={}, update=True):
+        self.update_shading(shading)
+        points = points.astype("float32", copy=False)
+        g2 = BufferGeometry(attributes={
+                "position": BufferAttribute(points, normalized=False)})
+        points = Points(geometry=g2, material=PointsMaterial(color=self.s["point_color"], 
+                                                             size=self.s["point_size"]))
+        self.mesh.add(points)
+        if update:
+            self.update()
+    
+    def add_edges(self, vertices, edges, shading={}, update=True):
+        self.update_shading(shading)
+        lines = np.zeros((edges.size, 3))
+        cnt = 0
+        for e in edges:
+            lines[cnt, :] = vertices[e[0]]
+            lines[cnt+1, :] = vertices[e[1]]
+            cnt += 2
+        lines = lines.astype("float32", copy=False)
+        g1 = BufferGeometry(attributes={
+                'position': BufferAttribute(lines, normalized=False),
+            },)
+
+        lines = LineSegments(geometry=g1,
+             material=LineBasicMaterial(linewidth=self.s["line_width"], color=self.s["line_color"]))
+        self.mesh.add(lines)
         if update:
             self.update()
     
@@ -164,15 +211,21 @@ class Viewer():
         self.mesh.add(arrowhelper)
 
         
-    def add_widget(self, widget, callback):
-        self.widgets.append(widget)
-        widget.observe(callback, names='value')
+    #def add_widget(self, widget, callback):
+    #    self.widgets.append(widget)
+    #    widget.observe(callback, names='value')
 
     def add_dropdown(self, options, default, desc, cb):
         widget = widgets.Dropdown(options=options, value=default, description=desc)
         self.widgets.append(widget)
         widget.observe(cb, names="value")
         display(widget)
+        
+    def add_button(self, text, cb):
+        button = widgets.Button(description=text)
+        self.widgets.append(button)
+        button.on_click(cb)
+        display(button)
 
     def launch(self):
         display(self.renderer)
@@ -181,19 +234,13 @@ class Viewer():
 
 
 def plot(v, f, c=None, shading={}, plot=None, return_plot=False):
-    shad = {"flat":True, "wireframe":True, "wire_width": 0.03}
-    for k in shading:
-        shad[k] = shading[k]
-    shading = shad
     if not plot:
-        view = Viewer()
+        view = Viewer(shading)
     else:
         view = plot
-    if c is not None:
-        c = get_colors(c, normalize=True)
     view.set_mesh(v, f, c, shading=shading)
     if not plot:
         view.launch()
-    #view.cam.zoom = view.scale
+
     if return_plot:
         return view
