9a29838d7c9a96e3b5cd268a3c2509c6855389e5
KarlLeell <cl3940@nyu.edu>
2019-07-17 22:02:43 -0400

    add per_face_normals per_vertex_attribute_smoothing piecewise_constant_winding_number

--- src/per_vertex_attribute_smoothing.cpp
+++ src/per_vertex_attribute_smoothing.cpp
@@ -1,16 +1,21 @@
+// TODO: __example
+
 #include <npe.h>
 #include <typedefs.h>
 #include <igl/per_vertex_attribute_smoothing.h>
 
 const char* ds_per_vertex_attribute_smoothing = R"igl_Qu8mg5v7(
 
+Smooth vertex attributes using uniform Laplacian
+
 Parameters
 ----------
-
+Ain  #V by #A eigen Matrix of mesh vertex attributes (each vertex has #A attributes)
+F    #F by 3 eigne Matrix of face (triangle) indices
 
 Returns
 -------
-
+Aout #V by #A eigen Matrix of mesh vertex attributes
 
 See also
 --------
@@ -23,12 +28,6 @@ None
 Examples
 --------
 
- Smooth vertex attributes using uniform Laplacian
-   Inputs:
-     Ain  #V by #A eigen Matrix of mesh vertex attributes (each vertex has #A attributes)
-     F    #F by 3 eigne Matrix of face (triangle) indices
-   Output:
-     Aout #V by #A eigen Matrix of mesh vertex attributes
 )igl_Qu8mg5v7";
 
 npe_function(per_vertex_attribute_smoothing)
@@ -40,8 +39,10 @@ npe_arg(f, dense_int, dense_long)
 
 npe_begin_code()
 
-  EigenDense<npe_Scalar_> aout;
-  igl::per_vertex_attribute_smoothing(ain, f, aout);
+  // remove __copy
+  Eigen::MatrixXd ain_copy = ain.template cast<double>();
+  Eigen::MatrixXd aout;
+  igl::per_vertex_attribute_smoothing(ain_copy, f, aout);
   return npe::move(aout);
 
 npe_end_code()
