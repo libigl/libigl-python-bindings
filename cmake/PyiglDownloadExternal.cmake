e643cfe46fc77fd5e2cff5d32c601b952471b06c
Teseo Schneider <teseo@bluewin.ch>
2019-08-08 14:41:14 +0200

    igl merge

--- cmake/PyiglDownloadExternal.cmake
+++ cmake/PyiglDownloadExternal.cmake
@@ -27,7 +27,7 @@ endfunction()
 function(pyigl_download_igl)
     pyigl_download_project(libigl
         GIT_REPOSITORY https://github.com/skoch9/libigl.git
-        GIT_TAG        0bb49a9f77a9431ff16312c89689be2a2c53c718
+        GIT_TAG        894703eba85c07d300dbb7e593778427f42abd24
     )
 endfunction()
 
