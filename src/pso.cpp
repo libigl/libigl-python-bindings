ea02e4e733de23fb2e3ed0098b4becea594b6723
Sebastian Koch <s.koch@phusion.eu>
2020-06-03 18:51:43 +0200

    Not used pso_periodic function

--- src/pso.cpp
+++ src/pso.cpp
@@ -61,57 +61,57 @@ npe_end_code()
 
 
 
-const char *ds_psop = R"igl_Qu8mg5v7ssp(
-Solve the problem:
+// const char *ds_psop = R"igl_Qu8mg5v7ssp(
+// Solve the problem:
+//
+//   minimize f(x)
+//   subject to lb ≤ x ≤ ub
+//
+// by particle swarm optimization (PSO).
+//
+//
+// Parameters
+// ----------
+// f  function that evaluates the objective for a given "particle" location
+// LB  #X vector of lower bounds
+// UB  #X vector of upper bounds
+// P  whether each DOF is periodic
+// max_iters  maximum number of iterations
+// population  number of particles in swarm
+//
+// Returns
+// -------
+// X  best particle seen so far
+// Returns objective corresponding to best particle seen so far
+//
+// See also
+// --------
+//
+//
+// Notes
+// -----
+// None
+//
+// Examples
+// --------
+//
+// )igl_Qu8mg5v7ssp";
 
-  minimize f(x)
-  subject to lb ≤ x ≤ ub
-
-by particle swarm optimization (PSO).
-
-
-Parameters
-----------
-f  function that evaluates the objective for a given "particle" location
-LB  #X vector of lower bounds
-UB  #X vector of upper bounds
-P  whether each DOF is periodic
-max_iters  maximum number of iterations
-population  number of particles in swarm
-
-Returns
--------
-X  best particle seen so far
-Returns objective corresponding to best particle seen so far
-
-See also
---------
-
-
-Notes
------
-None
-
-Examples
---------
-
-)igl_Qu8mg5v7ssp";
-
-npe_function(pso_periodic)
-npe_doc(ds_psop)
-
-npe_arg(f, const std::function< typename EigenDenseFloat::Scalar (Eigen::Matrix<typename EigenDenseFloat::Scalar, Eigen::Dynamic, 1> &) >)
-npe_arg(lb, EigenDenseFloat)
-npe_arg(ub, EigenDenseFloat)
-npe_arg(p, EigenDenseFloat)
-npe_arg(max_iters, int)
-npe_arg(population, int)
-
-
-npe_begin_code()
-  Eigen::Matrix<typename EigenDenseFloat::Scalar, Eigen::Dynamic, 1> x_copy;
-  const auto obj = igl::pso(f, lb, ub, p, max_iters, population, x_copy);
-  EigenDenseFloat x = x_copy;
-  return std::make_tuple(obj, npe::move(x));
-
-npe_end_code()
+// npe_function(pso_periodic)
+// npe_doc(ds_psop)
+//
+// npe_arg(f, const std::function< typename EigenDenseFloat::Scalar (Eigen::Matrix<typename EigenDenseFloat::Scalar, Eigen::Dynamic, 1> &) >)
+// npe_arg(lb, EigenDenseFloat)
+// npe_arg(ub, EigenDenseFloat)
+// npe_arg(p, EigenDenseFloat) //SHOULD BE BOOLEAN, BUT IS NOT USED ANYWAYS
+// npe_arg(max_iters, int)
+// npe_arg(population, int)
+//
+//
+// npe_begin_code()
+//   Eigen::Matrix<typename EigenDenseFloat::Scalar, Eigen::Dynamic, 1> x_copy;
+//   const auto obj = igl::pso(f, lb, ub, p, max_iters, population, x_copy);
+//   EigenDenseFloat x = x_copy;
+//   return std::make_tuple(obj, npe::move(x));
+//
+// npe_end_code()
