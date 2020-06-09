8e4e6a17ac47a8c12b47b0bbdd0ab2d5cff1c0c7
Soboleva Natalia <n.sob55@gmail.com>
2020-06-09 12:43:09 +0300

    switched back to dense_long for windows compatability

--- src/flip_edge.cpp
+++ src/flip_edge.cpp
@@ -46,7 +46,7 @@ Examples
 npe_function(flip_edge)
 npe_doc(ds_flip_edge)
 
-npe_arg(f, dense_longlong)
+npe_arg(f, dense_long)
 npe_arg(e, npe_matches(f))
 npe_arg(u_e, npe_matches(f))
 npe_arg(emap, npe_matches(f))
