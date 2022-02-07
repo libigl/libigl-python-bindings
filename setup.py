ab9c00394d455e3d70de79e8529f3e542bfec160
Teseo Schneider <teseo@bluewin.ch>
2022-02-07 14:00:17 -0800

    fixed ninja setup

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ class CMakeBuild(build_ext):
 
         if platform.system() == "Windows":
             cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
-            if os.environ.get('CMAKE_GENERATOR') != "NMake Makefiles":
+            if os.environ.get('CMAKE_GENERATOR') != "NMake Makefiles" and os.environ.get('CMAKE_GENERATOR') != "Ninja":
                 if sys.maxsize > 2**32:
                     cmake_args += ['-A', 'x64']
                 # build_args += ['--', '/m']
