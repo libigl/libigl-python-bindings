3b7e7466b7ab9034647b5690e30f155435caa387
Oras Phongpanangam <panangam@gmail.com>
2025-11-30 16:05:12 -0500

    add docstring

--- src/boundary_conditions.cpp
+++ src/boundary_conditions.cpp
@@ -21,7 +21,10 @@ namespace pyigl
 
     Eigen::VectorXI b;
     Eigen::MatrixXN bc;
-    igl::boundary_conditions(V, Ele, C, P, BE, CE, CF, b, bc);
+    if(!igl::boundary_conditions(V, Ele, C, P, BE, CE, CF, b, bc))
+    {
+      throw std::runtime_error("boundary_conditions: failed to compute");
+    }
     return std::make_tuple(b, bc);
   }
 }
@@ -38,6 +41,27 @@ void bind_boundary_conditions(nb::module_ &m)
     "BE"_a=Eigen::MatrixXI(),
     "CE"_a=Eigen::MatrixXI(),
     "CF"_a=Eigen::MatrixXI(),
-    R"(
-Compute boundary conditions for automatic weights computation. WIP)");
+R"(Compute boundary conditions for automatic weights computation. This
+function expects that the given mesh (V,Ele) has sufficient samples
+(vertices) exactly at point handle locations and exactly along bone and
+cage edges/faces.
+@param[in] V  #V by dim list of domain vertices
+@param[in] Ele  #Ele by simplex-size list of simplex indices
+@param[in] C  #C by dim list of handle positions
+@param[in] P  #P by 1 list of point handle indices into C
+@param[in] BE  #BE by 2 list of bone edge indices into C
+@param[in] CE  #CE by 2 list of cage edge indices into *P*
+@param[in] CF  #CF by 3 list of (triangular) cage face indices into *P*
+@param[out] b  #b list of boundary indices (indices into V of vertices which have
+    known, fixed values)
+@param[out] bc #b by #weights list of known/fixed values for boundary vertices
+    (notice the #b != #weights in general because #b will include all the
+    intermediary samples along each bone, etc.. The ordering of the
+    weights corresponds to [P;BE]
+throws error if boundary conditions are suspicious:
+  P and BE are empty
+  bc is empty
+  some column of bc doesn't have a 0 (assuming bc has >1 columns)
+  some column of bc doesn't have a 1 (assuming bc has >1 columns))"
+  );
 }
